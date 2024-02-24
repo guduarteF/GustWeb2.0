b762b4a911a005abc4ccef8b1ba24fcdaf2924be
Gustavo Duarte Ferreira g <gustavo64duarte@gmail.com>
2024-02-23 21:21:40 -0300

    DataTables API

--- GustWeb/Areas/Admin/Controllers/ProductController.cs
+++ GustWeb/Areas/Admin/Controllers/ProductController.cs
@@ -24,7 +24,7 @@ namespace GustWeb.Areas.Admin.Controllers
 
         public IActionResult Index()
         {
-            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
            
             return View(objProductList);
         }
@@ -69,6 +69,16 @@ namespace GustWeb.Areas.Admin.Controllers
                     string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
                     string productPath = Path.Combine(wwwRootPath, @"images\product");
 
+                    if (!string.IsNullOrEmpty(productVM.Product.ImageUrl))
+                    {
+                        //DELETE THE OLD IMAGE
+                        var oldImagePath = Path.Combine(wwwRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
+
+                        if(System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
                     using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
                     {
                         file.CopyTo(fileStream);
@@ -77,7 +87,16 @@ namespace GustWeb.Areas.Admin.Controllers
                     productVM.Product.ImageUrl = @"\images\product\" + fileName;
                 }
 
-                _unitOfWork.Product.Add(productVM.Product);
+                if(productVM.Product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(productVM.Product);
+                    
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(productVM.Product);
+                }
+
                 _unitOfWork.Save();
                 TempData["success"] = "Product created sucessfully";
                 return RedirectToAction("Index");
@@ -169,7 +188,16 @@ namespace GustWeb.Areas.Admin.Controllers
 
         }
 
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+            return Json(new { data = objProductList });
+            #endregion
+
+        }
     }
 
-
+  
 }
